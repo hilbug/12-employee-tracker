152f129d62bc8cd2fed9701748221b08609bc0ae
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-07 12:44:07 -0400

    update queries obj

--- src/config/queries.js
+++ src/config/queries.js
@@ -2,7 +2,9 @@ const path = require('path');
 const fs = require('fs');
 
 const queries = {
-    viewAllEmployees: fs.readFileSync(path.join(__dirname, '../sql/viewAllEmployees.sql')).toString()
+    viewAllEmployees: fs.readFileSync(path.join(__dirname, '../sql/viewAllEmployees.sql')).toString(),
+    viewAllDepartments: fs.readFileSync(path.join(__dirname, '../sql/viewAllDepartments.sql')).toString(),
+    viewAllRoles: fs.readFileSync(path.join(__dirname, '../sql/viewAllRoles.sql')).toString()
 }
 
 
