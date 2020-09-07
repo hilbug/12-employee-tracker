386578df67db2ab2b370f15273b582e2279b2d0f
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-07 14:20:15 -0400

    add actionsList

---
+++ src/lib/actionslist.js
@@ -0,0 +1,10 @@
+// store array of possible actions
+
+const actionsList = [
+    'View All Employees',
+    'View All Departments',
+    'View All Roles',
+    'Exit'
+];
+
+module.exports = { actionsList }
