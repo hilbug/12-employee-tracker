2e5190ee7676a15f28463b1b73f2c7963176ad7a
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-07 12:22:13 -0400

    add view all emp query

---
+++ src/sql/viewAllEmployees.sql
@@ -0,0 +1,14 @@
+SELECT
+	emp.id,
+	emp.firstname,
+	emp.lastname,
+	role.title,
+	dept.name department,
+	role.salary,
+	concat(manager.firstname, " ", manager.lastname) manager_name
+	
+FROM
+    ((employee emp
+    LEFT JOIN employee manager ON manager.id = emp.manager_id)
+    LEFT JOIN role on emp.role_id = role.id
+    LEFT JOIN department dept ON role.department_id = dept.id)
