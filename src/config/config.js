ea6b6bc0fa3364e32ceb0afd01f6d860fd60d54c
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-08 21:31:54 -0400

    remove multiplestatements

--- src/config/config.js
+++ src/config/config.js
@@ -4,8 +4,7 @@ const connection = mysql.createConnection({
 	host:"localhost",
 	user:"root",
 	password:"root12345",
-	database:"employeetracker",
-	multipleStatements: true
+	database:"employeetracker"
 });
 
 module.exports = connection;
