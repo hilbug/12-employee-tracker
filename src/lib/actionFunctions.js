e0ca955923f1cadb2b8fae9cee18cbf0e5d35775
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-08 22:03:49 -0400

    addEmployee, use try/catch, promisify

--- src/lib/actionFunctions.js
+++ src/lib/actionFunctions.js
@@ -8,6 +8,9 @@ const consoleTable = require('console.table');
 const inquirer = require("inquirer");
 // require actionslist
 const actionsList = require('./actionslist');
+// bring this in to use promises for connection.query
+const util = require("util");
+const query = util.promisify(connection.query).bind(connection);
 
 // function to initialize the app
 const start = () => {
@@ -38,8 +41,13 @@ const start = () => {
                         viewAllRolesQ();
                         break;
 
-                    // View all roles
+                    // Add an employee
                     case actionsList.actionsList[3]:
+                        addEmployeeQ();
+                        break;
+
+                    // Add a department
+                    case actionsList.actionsList[4]:
                         addDepartmentQ();
                         break;
                 }
@@ -48,31 +56,155 @@ const start = () => {
         });
 }
 
+// GET FUNCTIONS
+// get roles for inquirer choices
+const getRoles = () => {
+    return query(queries.viewAllRoles);
+}
+
+const getEmployees = () => {
+    return query(queries.viewAllEmployees);
+}
+
+const getDepartments = () => {
+    return query(queries.viewAllDepartments);
+}
+
+const getManagers = () => {
+    return query(queries.viewAllManagers);
+}
+
+// VIEW FUNCTIONS
 // View all employees function
-const viewAllEmployeesQ = () => {
-    connection.query(queries.viewAllEmployees, (err, res) => {
-        if (err) throw err;
-        console.table(res);
+const viewAllEmployeesQ = async () => {
+    try {
+        const rows = await getEmployees();
+        console.table(rows);
         start();
-    });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 // View all depts function
-const viewAllDeptsQ = () => {
-    connection.query(queries.viewAllDepartments, (err, res) => {
-        if (err) throw err;
-        console.table(res);
+const viewAllDeptsQ = async () => {
+    try {
+        const rows = await getDepartments();
+        console.table(rows);
         start();
-    });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 // View all roles function
-const viewAllRolesQ = () => {
-    connection.query(queries.viewAllRoles, (err, res) => {
-        if (err) throw err;
-        console.table(res);
+const viewAllRolesQ = async () => {
+    try {
+        const rows = await getRoles();
+        console.table(rows);
         start();
-    });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// ADD FUNCTIONS
+// Add an employee
+const addEmployeeQ = async () => {
+    try {
+        // define questions first
+        const promptUser = () => {
+            return inquirer
+                .prompt([
+                    {
+                        name: "empFirstName",
+                        type: "input",
+                        message: "Please enter a FIRST NAME for the new employee.",
+                    },
+                    {
+                        name: "empLastName",
+                        type: "input",
+                        message: "Please enter a LAST NAME for the new employee.",
+                    },
+                    {
+                        name: "empManagerYN",
+                        type: "rawlist",
+                        message: "Is the new employee a people manager? Select N for No or Y for Yes.",
+                        choices: ["N", "Y"]
+                    },
+                    {
+                        name: "empRoleId",
+                        type: "rawlist",
+                        choices: function () {
+                            const choiceArray = [];
+                            roles.forEach((role) => {
+                                const roleObj = {
+                                    name: role.title,
+                                    value: role.id
+                                }
+                                choiceArray.push(roleObj)
+                            })
+                            return choiceArray;
+                        },
+                        message: "Select a role for the new employee."
+
+                    },
+                    {
+                        name: "empManagerId",
+                        type: "rawlist",
+                        choices: function () {
+                            const choiceArray = [];
+                            managers.forEach((mgr) => {
+                                const mgrObj = {
+                                    name: mgr.name,
+                                    value: mgr.id
+                                }
+                                choiceArray.push(mgrObj)
+                            })
+                            return choiceArray;
+                        },
+                        message: "Select a manager for the new employee."
+
+                    }
+                ])
+                .then((answer) => {
+                    console.log(answer.empFirstName);
+                    console.log(answer.empLastName);
+                    console.log(answer.empRoleId);
+                    console.log(answer.empManagerYN);
+                    console.log(answer.empManagerId);
+                    //console.log("this is undefined " + chosenRole);
+                    //console.log(getChosenRoleId(answer.empRole));
+                    //start comment below
+
+                    connection.query(
+                        queries.addEmployee,
+                        {
+                            firstname: answer.empFirstName,
+                            lastname: answer.empLastName,
+                            role_id: answer.empRoleId,
+                            manager_id: answer.empManagerId,
+                            manageryn: answer.empManagerYN
+                        },
+                        (err) => {
+                            if (err) throw err;
+                            console.log(`The new employee ${answer.empFirstName} ${answer.empLastName} was added successfully!`);
+                            start();
+                        });
+                });
+        }
+
+        // await functions 
+        const roles = await getRoles();
+        //console.log(roles);
+        const managers = await getManagers();
+        //console.log(managers);
+        await promptUser();
+
+    } catch (err) {
+        console.log(err);
+    }
+
 }
 
 // Add department
