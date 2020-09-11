86a1e356cb6e359bb51e0e96c6cfc6b1b137115a
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-10 22:15:24 -0400

    add ifnull to view employees by mgr

--- src/sql/viewEmployeesByManager.sql
+++ src/sql/viewEmployeesByManager.sql
@@ -1,14 +1,13 @@
 SELECT
-	CONCAT(manager.firstname, " ", manager.lastname) manager_name,
+	IFNULL(CONCAT(manager.firstname, " ", manager.lastname),'No Manager') manager_name,
     GROUP_CONCAT(
         emp.firstname, " ", emp.lastname
         SEPARATOR ', '
     ) employees
-	
 FROM
     ((employee emp
     LEFT JOIN employee manager ON manager.id = emp.manager_id))
 GROUP BY
-    concat(manager.firstname, " ", manager.lastname)
+    IFNULL(CONCAT(manager.firstname, " ", manager.lastname),'No Manager')
 ORDER BY
 	manager_name
